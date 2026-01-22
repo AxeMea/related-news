035dc2777440ade2141e09d5069e2e3276cefe77
yuzheng <yuzheng@daojia-inc.com>
2026-01-22 10:08:12 +0800

    自动提交

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+import { execSync } from 'child_process'
+
+const loop = () => {
+    console.log(`自动提交 ${new Date().toLocaleString()}`)
+
+    execSync('git add .')
+    execSync('git commit -m "自动提交"')
+
+    // 执行 git push 命令
+    execSync('git push origin main')
+}
+
+setInterval(() => {
+    loop()
+}, 1000 * 60 * 30)
+
+loop()
